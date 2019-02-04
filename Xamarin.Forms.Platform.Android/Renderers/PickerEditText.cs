65429973def9eb23b601e45fb0b60a717978fb97
ShaneN <shane94@hotmail.com>
2019-02-04 16:44:32 -0700

    Generalize PickerEditText

--- Xamarin.Forms.Platform.Android/Renderers/PickerEditText.cs
+++ Xamarin.Forms.Platform.Android/Renderers/PickerEditText.cs
@@ -6,38 +6,54 @@ using Java.Lang;
 using System.Collections.Generic;
 using Android.Graphics;
 using Android.Runtime;
+using AView = global::Android.Views.View;
+using System;
 
 namespace Xamarin.Forms.Platform.Android
 {
-	public class PickerEditText : EditText, IPopupTrigger
+	public static class PickerEditTextManager
 	{
 		readonly static HashSet<Keycode> availableKeys = new HashSet<Keycode>(new[] {
 			Keycode.Tab, Keycode.Forward, Keycode.Back, Keycode.DpadDown, Keycode.DpadLeft, Keycode.DpadRight, Keycode.DpadUp
 		});
 
-		System.WeakReference<IPickerRenderer> rendererRef;
-
-		public bool ShowPopupOnFocus { get; set; }
-
-		public PickerEditText(Context context, IPickerRenderer pickerRenderer) : base(context)
+		public static void Setup(Context context, EditText editText)
 		{
-			Focusable = true;
-			Clickable = true;
-			InputType = InputTypes.Null;
-			KeyPress += OnKeyPress;
-			rendererRef = new System.WeakReference<IPickerRenderer>(pickerRenderer);
-			SetOnClickListener(PickerListener.Instance);
+			editText.Focusable = true;
+			editText.Clickable = true;
+			editText.InputType = InputTypes.Null;
+			editText.KeyPress += OnKeyPress;
+			editText.FocusChange += OnFocusChange;
+			editText.Touch += OnTouchEvent;
+			editText.SetOnClickListener(PickerListener.Instance);
 		}
 
-		protected override void OnFocusChanged(bool gainFocus, [GeneratedEnum] FocusSearchDirection direction, Rect previouslyFocusedRect)
+		static void OnTouchEvent(object sender, AView.TouchEventArgs e)
 		{
-			base.OnFocusChanged(gainFocus, direction, previouslyFocusedRect);
-			if (gainFocus && ShowPopupOnFocus)
-				CallOnClick();
-			ShowPopupOnFocus = false;
+			if (sender is PickerEditText view)
+			{
+				if (e.Event.Action == MotionEventActions.Up && !view.IsFocused)
+				{
+					view.RequestFocus();
+				}
+
+				e.Handled = false;
+			}
 		}
 
-		void OnKeyPress(object sender, KeyEventArgs e)
+		static void OnFocusChange(object sender, AView.FocusChangeEventArgs e)
+		{
+			if (sender is AView view && sender is IPopupTrigger popupTrigger)
+			{
+				// Todo check if gain focus is different
+				if (e.HasFocus && popupTrigger.ShowPopupOnFocus)
+					view.CallOnClick();
+
+				popupTrigger.ShowPopupOnFocus = false;
+			}
+		}
+
+		static void OnKeyPress(object sender, AView.KeyEventArgs e)
 		{
 			if (availableKeys.Contains(e.KeyCode))
 			{
@@ -45,27 +61,18 @@ namespace Xamarin.Forms.Platform.Android
 				return;
 			}
 			e.Handled = true;
-			CallOnClick();
+			(sender as AView)?.CallOnClick();
 		}
 
-		public override bool OnTouchEvent(MotionEvent e)
+		public static void Dispose(EditText editText)
 		{
-			if (e.Action == MotionEventActions.Up && !IsFocused)
-				RequestFocus();
-			return base.OnTouchEvent(e); // raises the OnClick event if focus is already received
+			editText.KeyPress -= OnKeyPress;
+			editText.FocusChange -= OnFocusChange;
+			editText.Touch -= OnTouchEvent;
+			editText.SetOnClickListener(null);
 		}
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				KeyPress -= OnKeyPress;
-				rendererRef = null;
-			}
-			base.Dispose(disposing);
-		}
-
-		class PickerListener : Object, IOnClickListener
+		class PickerListener : global::Java.Lang.Object, AView.IOnClickListener
 		{
 			public static readonly PickerListener Instance = new PickerListener();
 
@@ -73,10 +80,32 @@ namespace Xamarin.Forms.Platform.Android
 			{
 				if (v is PickerEditText picker)
 				{
-					picker.rendererRef.TryGetTarget(out IPickerRenderer renderer);
-					renderer?.OnClick();
+					if (picker?.Parent is IPickerRenderer renderer1)
+						renderer1.OnClick();
+					else if (picker?.Parent?.Parent is IPickerRenderer renderer2)
+						renderer2.OnClick();
+					else
+						throw new System.Exception("Renderer not found temp check for Shane things");
 				}
 			}
+		}
+	}
+
+
+	public class PickerEditText : EditText, IPopupTrigger
+	{
+		public bool ShowPopupOnFocus { get; set; }
+
+		public PickerEditText(Context context, IPickerRenderer pickerRenderer) : base(context)
+		{
+			PickerEditTextManager.Setup(Context, this);
+		}
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+				PickerEditTextManager.Dispose(this);
+
+			base.Dispose(disposing);
 		}
 	}
 }
